6d9b7fbc6943f0e1e0c810bb9ec8e8b64fb27d9b
Karl Maghariki <thekarlmagar@gmail.com>
2020-12-06 16:22:35 +0200

    uploading game

---
+++ javascript/app.js
@@ -0,0 +1,38 @@
+function winEasy() {
+  alert("You won !");
+  let answer = prompt("Did you enjoy the easy level ?")
+  answer = answer.trim().toLowerCase();   
+  if (answer === "yes") {
+    alert("then try the medium level!")
+  } else if (answer === "no") {
+    alert("Oh! Anyway, have a nice day")
+  } else {
+    alert("please, choose between yes or no.")
+  }
+}
+
+function winMedium() {
+  alert("You won !");
+  let answer = prompt("Did you enjoy the medium level ?")
+  answer = answer.trim().toLowerCase();   
+  if (answer === "yes") {
+    alert("then try the hard level!")
+  } else if (answer === "no") {
+    alert("Oh! Anyway, have a nice day")
+  } else {
+    alert("please, choose between yes or no.")
+  }
+}
+
+function winHard() {
+  alert("You won !");
+  let answer = prompt("Did you enjoy the hard level ?")
+  answer = answer.trim().toLowerCase();   
+  if (answer === "yes") {
+    alert("You're a beast!")
+  } else if (answer === "no") {
+    alert("Oh! Anyway, have a nice day")
+  } else {
+    alert("please, choose between yes or no.")
+  }
+}
