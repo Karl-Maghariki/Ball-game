26fa740b94ca80c20d077f1fbba71e5c54647a27
Karl Maghariki <thekarlmagar@gmail.com>
2020-12-07 21:08:49 +0200

    finished basic js

--- javascript/app.js
+++ javascript/app.js
@@ -5,3 +5,37 @@ function startGame() {
 function stopGame() {
   document.getElementById("ball").style.animationDuration = "0s"
 }
+
+function mediumLevel() {
+  document.getElementById("ball").style.animationDuration = "3.8s";
+}
+
+function hardLevel() {
+  document.getElementById("ball").style.animationDuration = "3s";
+}
+
+function veryHardLevel() {
+  document.getElementById("ball").style.animationDuration = "2s";
+}
+
+function godModeLevel() {
+  document.getElementById("ball").style.animationDuration = "1s";
+}
+
+function impossibleLevel() {
+  document.getElementById("ball").style.animationDuration = "0.4s";
+}
+
+function smallBall() {
+  document.getElementById("ball").style.height = "50px";
+  document.getElementById("ball").style.width = "50px";
+}
+
+function mediumBall() {
+  document.getElementById("ball").style.height = "75px";
+  document.getElementById("ball").style.width = "75px";
+}
+function bigBall() {
+  document.getElementById("ball").style.height = "100px";
+  document.getElementById("ball").style.width = "100px";
+}
