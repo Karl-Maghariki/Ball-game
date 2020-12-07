cd521e9a0b7202b8d78a3232603a44bb5848c186
Karl Maghariki <thekarlmagar@gmail.com>
2020-12-07 20:56:50 +0200

    began js

--- javascript/app.js
+++ javascript/app.js
@@ -1,38 +1,7 @@
-function winEasy() {
-  alert("You won !");
-  let answer = prompt("Did you enjoy the easy level ?")
-  answer = answer.trim().toLowerCase();   
-  if (answer === "yes") {
-    alert("then try the medium level!")
-  } else if (answer === "no") {
-    alert("Oh! Anyway, have a nice day")
-  } else {
-    return alert("please, choose between yes or no.")
-  }
+function startGame() {
+  document.getElementById("ball").style.animationDuration = "5s"
 }
 
-function winMedium() {
-  alert("You won !");
-  let answer = prompt("Did you enjoy the medium level ?")
-  answer = answer.trim().toLowerCase();   
-  if (answer === "yes") {
-    alert("then try the hard level!")
-  } else if (answer === "no") {
-    alert("Oh! Anyway, have a nice day")
-  } else {
-    return alert("please, choose between yes or no.")
-  }
-}
-
-function winHard() {
-  alert("You won !");
-  let answer = prompt("Did you enjoy the hard level ?")
-  answer = answer.trim().toLowerCase();   
-  if (answer === "yes") {
-    alert("You're a beast!")
-  } else if (answer === "no") {
-    alert("Oh! Anyway, have a nice day")
-  } else {
-    return alert("please, choose between yes or no.")
-  }
+function stopGame() {
+  document.getElementById("ball").style.animationDuration = "0s"
 }
