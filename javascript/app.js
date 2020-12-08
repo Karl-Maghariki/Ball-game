f4e1949184f8ffcc557aa342ac070717d22c7fda
Karl Maghariki <thekarlmagar@gmail.com>
2020-12-08 18:35:14 +0200

    finalize progject

--- javascript/app.js
+++ javascript/app.js
@@ -1,41 +1,48 @@
+//announce variables
+let ball = document.getElementById("ball");
+
 function startGame() {
-  document.getElementById("ball").style.animationDuration = "5s"
+  ball.style.animationDuration = "5s"
 }
 
 function stopGame() {
-  document.getElementById("ball").style.animationDuration = "0s"
+  ball.style.animationDuration = "0s"
 }
 
 function mediumLevel() {
-  document.getElementById("ball").style.animationDuration = "3.8s";
+  ball.style.animationDuration = "3.8s";
 }
 
 function hardLevel() {
-  document.getElementById("ball").style.animationDuration = "3s";
+  ball.style.animationDuration = "3s";
 }
 
 function veryHardLevel() {
-  document.getElementById("ball").style.animationDuration = "2s";
+  ball.style.animationDuration = "2s";
 }
 
 function godModeLevel() {
-  document.getElementById("ball").style.animationDuration = "1s";
+  ball.style.animationDuration = "1s";
 }
 
 function impossibleLevel() {
-  document.getElementById("ball").style.animationDuration = "0.4s";
+  ball.style.animationDuration = "0.4s";
 }
 
 function smallBall() {
-  document.getElementById("ball").style.height = "50px";
-  document.getElementById("ball").style.width = "50px";
+  ball.style.height = "50px";
+  ball.style.width = "50px";
 }
 
 function mediumBall() {
-  document.getElementById("ball").style.height = "75px";
-  document.getElementById("ball").style.width = "75px";
+  ball.style.height = "75px";
+  ball.style.width = "75px";
 }
 function bigBall() {
-  document.getElementById("ball").style.height = "100px";
-  document.getElementById("ball").style.width = "100px";
+  ball.style.height = "100px";
+  ball.style.width = "100px";
+}
+
+function win() {
+  alert("You won!")
 }
