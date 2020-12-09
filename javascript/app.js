4743ab55c15bd50d56060d871457f4521ae739d1
Karl Maghariki <thekarlmagar@gmail.com>
2020-12-09 19:10:23 +0200

    almost done

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,39 @@
-let ball = document.getElementById("ball");function startGame() { ball.style.animationDuration = "5s"}function stopGame() {ball.style.animationDuration = "0s"}function mediumLevel() {ball.style.animationDuration = "3.8s";}function hardLevel() {ball.style.animationDuration = "3s";}function veryHardLevel() {ball.style.animationDuration = "2s";}function godModeLevel() {ball.style.animationDuration = "1s";}function impossibleLevel() {ball.style.animationDuration = "0.4s";}function smallBall() {ball.style.height = "50px";ball.style.width = "50px";}function mediumBall() {ball.style.height = "75px";ball.style.width = "75px";}function bigBall() {  ball.style.height = "100px";ball.style.width = "100px";}function winGame() {alert("You won!")}
+//announcing variables
+let ball = document.getElementById("ball");
+
+function startGame() {
+  ball.style.animationDuration = "5s";
+}
+function stopGame() {
+  ball.style.animationDuration = "0s";
+}
+function mediumLevel() {
+  ball.style.animationDuration = "3.8s";
+}
+function hardLevel() {
+  ball.style.animationDuration = "3s";
+}
+function veryHardLevel() {
+  ball.style.animationDuration = "2s";
+}
+function godModeLevel() {
+  ball.style.animationDuration = "1s";
+}
+function impossibleLevel() {
+  ball.style.animationDuration = "0.4s";
+}
+function smallBall() {
+  ball.style.height = "50px";
+  ball.style.width = "50px";
+}
+function mediumBall() {
+  ball.style.height = "75px";
+  ball.style.width = "75px";
+}
+function bigBall() {
+  ball.style.height = "100px";
+  ball.style.width = "100px";
+}
+function winGame() {
+  alert("You won!");
+}
