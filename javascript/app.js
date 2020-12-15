3b41830599b052a84cc2341604f7967ee19ccba3
Karl Maghariki <thekarlmagar@gmail.com>
2020-12-15 19:19:30 +0200

    added comments

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,10 @@
 //announcing variables
 let ball = document.getElementById("ball").style; //the ball that user must aim
 
+function startGame() {
+  setInterval(countDown, 3000) //set interval will repeat the action every 3000 ms (milliseconds)
+}
+
 function countDown() {
   let counter = 3;
   counter--;
